2ea4e6b8d3eb3e18178972e4b6afdd58d44123c1
YHonchak <yaroslav.honchak@gmail.com>
2026-07-26 19:34:53 +0300

    doc(main): added `todo` about entred number hiding

--- 3_3_hi_lo.cpp
+++ 3_3_hi_lo.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     int num{};
-    std::cout << "Enter a number: ";
+    std::cout << "Enter a number: "; // todo: need to discover how to hide entered number from the users (players)
     std::cin >> num;
 
     std::cout << "You entered: " << num << "\n";
