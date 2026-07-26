0b6f094b50beb56fa264b5bcab6fc2b08b8338f1
YHonchak <yaroslav.honchak@gmail.com>
2026-07-26 20:34:25 +0300

    feat(game): add logic to enter a number by player 1

--- 3_3_hi_lo.cpp
+++ 3_3_hi_lo.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     int numberToGuess{};
-    std::cout << "Enter a number: "; // todo: need to discover how to hide entered number from the users (players)
+    std::cout << "Enter a number to be guessed: "; // todo: need to discover how to hide entered number from the users (players)
     std::cin >> numberToGuess;
 
     std::string player_1{ "Player 1" };
@@ -12,4 +12,9 @@ int main()
     std::cout << player_1 << " and " << player_2 << " participated in the game." << "\n";
 
     int num1{}, num2{};
+
+    std::cout << player_1 << " please enter a number to guess: ";
+    std::cin >> num1;
+
+    std::cout << player_1 << " entered a number: " << num1;
 }
