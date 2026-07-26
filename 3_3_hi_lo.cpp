377500506d3cb3ce6a2ba7528d6b8a7836fb36ea
YHonchak <yaroslav.honchak@gmail.com>
2026-07-26 20:46:47 +0300

    feat(game): check the `player_1` attempt

--- 3_3_hi_lo.cpp
+++ 3_3_hi_lo.cpp
@@ -9,12 +9,20 @@ int main()
     std::string player_1{ "Player 1" };
     std::string player_2{ "Player 2" };
 
-    std::cout << player_1 << " and " << player_2 << " are participating in the game." << "\n";
+    std::cout << player_1 << " and " << player_2 << " are participating in the game." << "\n\n";
 
     int num1{}, num2{};
 
     std::cout << player_1 << " please enter a number to guess: ";
     std::cin >> num1;
 
-    std::cout << player_1 << " entered a number: " << num1;
+    if (numberToGuess > num1) {
+        std::cout << "Guessed number is HIGHER than " << num1;
+    }
+    else if (numberToGuess < num1) {
+        std::cout << "Guessed number is LOWER than " << num1;
+    }
+    else {
+        std::cout << "Congratulations! " << player_1 << " has guessed a number and WON the game!";
+    }
 }
