293e3a0de662281fa9e3ee627297f15bcd05126d
YHonchak <yaroslav.honchak@gmail.com>
2026-07-27 11:49:09 +0300

    feat(check-attempt): add player_2 number check

--- 3_3_hi_lo.cpp
+++ 3_3_hi_lo.cpp
@@ -2,13 +2,13 @@
 
 bool checkAttempt(std::string player, int playerNum, int guessNum) {
     if (guessNum > playerNum) {
-        std::cout << "Guessed number is HIGHER than " << playerNum;
+        std::cout << "Guessed number is HIGHER than " << playerNum << "\n";
     }
     else if (guessNum < playerNum) {
-        std::cout << "Guessed number is LOWER than " << playerNum;
+        std::cout << "Guessed number is LOWER than " << playerNum << "\n";
     }
     else {
-        std::cout << "Congratulations! " << player << " has guessed a number and WON the game!";
+        std::cout << "Congratulations! " << player << " has guessed a number and WON the game!" << "\n";
     }
 
     return false;
@@ -16,14 +16,14 @@ bool checkAttempt(std::string player, int playerNum, int guessNum) {
 
 int main()
 {
-    int numberToGuess{};
-    std::cout << "Enter a number to be guessed: "; // todo: need to discover how to hide entered number from the users (players)
-    std::cin >> numberToGuess;
-
     std::string player_1{ "Player 1" };
     std::string player_2{ "Player 2" };
 
-    std::cout << player_1 << " and " << player_2 << " are participating in the game." << "\n\n";
+    std::cout << player_1 << " and " << player_2 << " are participating in the game." << "\n";
+
+    int numberToGuess{};
+    std::cout << "Enter a number to be guessed: "; // todo: need to discover how to hide entered number from the users (players)
+    std::cin >> numberToGuess;
 
     int num1{}, num2{};
 
@@ -31,6 +31,11 @@ int main()
     std::cin >> num1;
 
     checkAttempt(player_1, num1, numberToGuess);
+
+    std::cout << player_2 << " please enter a number to guess: ";
+    std::cin >> num2;
+
+    checkAttempt(player_2, num2, numberToGuess);
 
     return 0;
 }
