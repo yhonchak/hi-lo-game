6a824a59693ac3e95ce6d6ce21a7d06957cb2045
YHonchak <yaroslav.honchak@gmail.com>
2026-07-29 13:47:32 +0300

    refactor(checkAttempt): move player input logic to the function

--- 3_3_hi_lo.cpp
+++ 3_3_hi_lo.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 
 bool checkAttempt(std::string player, int playerNum, int guessNum) {
+    std::cout << player << " please enter a number to guess: ";
+    std::cin >> playerNum;
+    
     if (guessNum > playerNum) {
         std::cout << "Guessed number is HIGHER than " << playerNum << "\n";
     }
     else if (guessNum < playerNum) {
-        std::cout << "Guessed number is LOWER than " << playerNum << "\n";
+        std::cout << "Guessed number is LOWER than " << playerNum << "\n\n";
     }
     else {
-        std::cout << "Congratulations! " << player << " has guessed a number and WON the game!" << "\n";
+        std::cout << "Congratulations! " << player << " has guessed a number and WON the game!" << "\n\n";
         return true;
     }
 
@@ -28,13 +31,7 @@ int main()
 
     int num1{}, num2{};
 
-    std::cout << player_1 << " please enter a number to guess: ";
-    std::cin >> num1;
-
     checkAttempt(player_1, num1, numberToGuess);
-
-    std::cout << player_2 << " please enter a number to guess: ";
-    std::cin >> num2;
 
     checkAttempt(player_2, num2, numberToGuess);
 
