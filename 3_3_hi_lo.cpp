a10f1dcc207d399d5637b6c2158d3709670affcd
YHonchak <yaroslav.honchak@gmail.com>
2026-07-29 14:18:25 +0300

    feat(game): implement game process

--- 3_3_hi_lo.cpp
+++ 3_3_hi_lo.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 
-bool checkAttempt(std::string player, int playerNum, int guessNum) {
+int numberToGuess{};
+
+std::string player_1{ "Player 1" };
+std::string player_2{ "Player 2" };
+
+int num1{}, num2{};
+
+bool checkAttempt(std::string player, int playerNum) {
     std::cout << player << " please enter a number to guess: ";
     std::cin >> playerNum;
     
-    if (guessNum > playerNum) {
+    if (numberToGuess > playerNum) {
         std::cout << "Guessed number is HIGHER than " << playerNum << "\n\n";
     }
-    else if (guessNum < playerNum) {
+    else if (numberToGuess < playerNum) {
         std::cout << "Guessed number is LOWER than " << playerNum << "\n\n";
     }
     else {
@@ -18,26 +25,28 @@ bool checkAttempt(std::string player, int playerNum, int guessNum) {
     return false;
 }
 
-void game(std::string player1, std::string player2, int guessNum) {
-    int num1{}, num2{};
-
-    checkAttempt(player1, num1, guessNum);
-
-    checkAttempt(player2, num2, guessNum);
+void gameStep(std::string player, int num) {
+    if (!checkAttempt(player, num)) {
+        std::string currentPlayer = player_1;
+        int currentNum = num1;
+        
+        if (player == player_1) {
+            currentPlayer = player_2;
+            currentNum = num2;
+        }
+        gameStep(currentPlayer, currentNum);
+    }
 }
 
 int main()
 {
-    std::string player_1{ "Player 1" };
-    std::string player_2{ "Player 2" };
-
     std::cout << player_1 << " and " << player_2 << " are participating in the game." << "\n";
 
-    int numberToGuess{};
     std::cout << "Enter a number to be guessed: "; // todo: need to discover how to hide entered number from the users (players)
     std::cin >> numberToGuess;
+    std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 
-    game(player_1, player_2, numberToGuess);
+    gameStep(player_1, num1);
 
     return 0;
 }
